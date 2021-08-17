5ae156a04341dba0d39abb2c8934d0ce08aedc79
Enrique Piatti <enrique.piatti@gmail.com>
2021-08-17 12:51:57 -0300

    Tabla de jugadores y principio de generacion de líneas

---
+++ data.js
@@ -0,0 +1,67 @@
+let RUF_players = [
+    {
+        name: 'Jenry',
+        handler: false,
+        female: false,
+        speed: 5,
+        height: 174,
+    },
+    {
+        name: 'Lucy',
+        handler: true,
+        female: false,
+        speed: 5,
+        height: 174,
+    },
+    {
+        name: 'Cardu',
+        handler: true,
+        female: false,
+        speed: 5,
+    },
+    {
+        name: 'Uli',
+        handler: true,
+        female: false,
+    },
+    {
+        name: 'Scooby',
+        handler: true,
+        female: false,
+    },
+    {
+        name: 'Rodri',
+        handler: true,
+        female: false,
+    },
+    {
+        name: 'Orne',
+        handler: true,
+        female: true,
+    },
+    {
+        name: 'Negra',
+        handler: true,
+        female: true,
+    },
+    {
+        name: 'Dani',
+        handler: false,
+        female: true,
+    },
+    {
+        name: 'Lu',
+        handler: false,
+        female: true,
+    },
+    {
+        name: 'Bian',
+        handler: false,
+        female: true,
+    },
+    {
+        name: 'Oli',
+        handler: false,
+        female: true,
+    },
+];
